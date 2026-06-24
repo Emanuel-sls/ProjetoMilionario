550e11f8d1cbdb5a17bf80266a545b92811d8b94
Manu140510 <e.manuellysantos1405@gmail.com>
2026-06-24 18:40:47 -0300

    HTML/CSS da pág.Inicial Adm e Cliente. E Imgs
    No HTML adicionei uma fivecon coloquei a img na pasta correta.
    No CSS arrumei o background do button e coloquei a img na pasta correta.

--- index.php
+++ index.php
@@ -3,28 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/android-chrome-192x192.png">
     <link rel="stylesheet" href="style.css">
     <title>Sabor Vivo</title>
 </head>
 <body>
-     <div class="painel">
+
+    <div class="painel">
         <!-- Cabeçalho: Título e Subtítulo da Tela Inicial -->
         <header>
             <p class="acesso-digital">Acesso digital</p>
             <h1 class="titulo">Sabor Vivo</h1>
             <hr class="linhaA">
             <p class="subtitulo">Fresco, Feito e Servido</p>
-        </header>
+        </header><br>
 
         <!-- Tela principal: Botões um para cliente e outro adm -->
         <main>
             <button class="button" type="button">Cliente</button>
             <button class="button" type="button">Administrador</button>
-        </main>
-
+        </main><br>
+            
         <!-- Rodapé:Info. da Tela Inicial -->
         <footer>
-            Aberto todos os dias • 10:30 - 21:30<br>
+            Aberto todos os dias • 10:30 - 21:00<br>
             <hr class="linhaB">
             Rua das Flores, 123 • Centro, São Paulo<br>
             (11) 99999-0000<br>
