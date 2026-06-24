90d70ce2059f8828b862be385e158116a87d758a
Manu140510 <e.manuellysantos1405@gmail.com>
2026-06-24 19:03:34 -0300

    Tela inicial Cliente e ADM
    Adicionei a Tela inicial do Cliente e A tela de Cadastro e Login do ADM

---
+++ Restaurante-etec/Cliente/pagInicialCliente.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="inicialCliente.css">
+    <title>Sabor Vivo</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="img/android-chrome-192x192.png">
+</head>
+<body>
+    <div class="painel">
+        <!-- Cabeçalho: Título e Subtítulo da Tela Inicial -->
+        <header>
+            <p class="acesso-digital">Acesso digital</p>
+            <h1 class="titulo">Sabor Vivo</h1>
+            <hr class="linhaA">
+            <p class="subtitulo">Fresco, Feito e Servido</p><br>
+        </header>
+
+        <!-- Tela principal: Botões um para cliente e outro adm -->
+        <main>
+            <div class="imgcentral"><img src="img/imgcentral.png" alt="icon"></div><br>
+            Aberto todos os dias • 10:30 - 21:00<br>
+            <hr class="linhaB">
+            Rua das Flores, 123 • Centro, São Paulo<br>
+            (11) 99999-0000<br>
+        </main><br>
+
+        <!-- Rodapé:Info. da Tela Inicial -->
+        <footer>
+             <button class="button" type="button">Ver cardápio</button>
+        </footer>
+</body>
+</html>
