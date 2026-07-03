380f3fc1f3b9ef79398ef49119e4b15dde10e4ca
Pedro Ribeiro Deziderio <pedrodeziderio90@gmail.com>
2026-07-03 00:27:02 -0300

    js de ligação para dashboard cliente

--- Restaurante-etec/assets/js/ligacao_index.js
+++ Restaurante-etec/assets/js/ligacao_index.js
@@ -1,12 +1,13 @@
-//Aqui só pegando o local dos botão
+//Essa parte aqui pega todo os botões do index.php para fazer a troca de pagina
 const btnDirCliente = document.getElementById("btnCliente");
 const btnDirAdmin = document.getElementById("btnAdmin");
 
-//botando os botão pra funciona
+//Para Inicial cliente
 btnDirCliente.addEventListener("click", function(){
     location.href="Restaurante-etec/Cliente/pagInicialCliente.php"
 });
 
+//Para LoginAdmin
 btnDirAdmin.addEventListener("click", function(){
     location.href="Restaurante-etec/Admin/pagLoginadm.php"
 });
