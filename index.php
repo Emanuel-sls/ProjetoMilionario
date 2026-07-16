8d4e1bc2a610dfd4c0e6081ead10a4120a860ede
Emanuel Santiago <emanuelsantiago152133@gmail.com>
2026-07-16 18:23:43 -0300

    sdsd

--- index.php
+++ index.php
@@ -11,10 +11,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" sizes="32x32" href="Restaurante-etec/assets/img/android-chrome-192x192.png">
-    <link rel="stylesheet" href="Restaurante-etec/assets/css/style.css">
+    <link rel="stylesheet" href="/Projeto%20Milionario/ProjetoMilionario/Restaurante-etec/assets/css/style.css">
     <title>Sabor Vivo</title>
+      
 </head>
 <body>
+  
 
     <div class="painel">
         <!-- Cabeçalho: Título e Subtítulo da Tela Inicial -->
@@ -29,6 +31,7 @@
         <main>
             <button class="button" type="button" id="btnCliente">Cliente</button>
             <button class="button" type="button" id="btnAdmin">Administrador</button>
+
         </main><br>
             
         <!-- Rodapé:Info. da Tela Inicial -->
