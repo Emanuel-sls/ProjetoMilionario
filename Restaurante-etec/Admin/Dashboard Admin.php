cd3933e64a0348594de4425cd8c8b8c6417a85a2
Pedro Ribeiro Deziderio <pedrodeziderio90@gmail.com>
2026-08-18 19:06:29 -0300

    Update Dashboard Admin.php

--- Restaurante-etec/Admin/Dashboard Admin.php
+++ Restaurante-etec/Admin/Dashboard Admin.php
@@ -290,6 +290,7 @@ require_once __DIR__ . "/../includes/DeleteProduto.php";
 
                         <h3>Erro!</h3>
 
+                        <p id="campo_msgVazio"></p>
                         <button class="btn-erro">Tentar novamente</button>
                     </div>
 
